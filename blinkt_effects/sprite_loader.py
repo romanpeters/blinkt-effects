3c05bbdb6a9ff79732d6962a4b2f154334f315e3
Roman Peters <github@romanpeters.nl>
2020-11-04 23:56:34 +0100

    Add scrolling sprites

--- blinkt_effects/sprite_loader.py
+++ blinkt_effects/sprite_loader.py
@@ -3,9 +3,15 @@ import numpy as np
 
 
 class Sprite(object):
-    def __init__(self, path, gamma_correction=True, gamma=2.6):
+    def __init__(self, path, gamma_correction=True, gamma=2.6, num_leds=8):
         self.path = path
         self.img = Image.open(path)
+        self.num_leds = num_leds
+        if self.img.size[1] == 1:  # image is height is 1 pixel
+            self.scroll = True
+        else:
+            assert self.img.size[0] == num_leds, 'Image is a different width than the number of pixels'
+            self.scroll = False
         if gamma_correction:
             corrected = np.array(255 * (np.array(self.img) / 255) ** gamma, dtype='uint8')
             self.img = Image.fromarray(corrected, 'RGB')
@@ -21,7 +27,12 @@ class Sprite(object):
         for (r, g, b) in zip(self.red, self.green, self.blue):
             pixel = (r, g, b)
             line.append(pixel)
-            if len(line) == self.img.size[0]:  # image width reached
+            if len(line) == self.num_leds:  # image width reached
                 result.append(line)
-                line = []
+                if self.scroll:
+                    line = line.copy()
+                    line.pop(0)
+                else:
+                    line = []
+
         return result
